e8429ed319c1c318644ef2f76e0b9cbbf117810e
swfxz <fxzmythus@gmail.com>
2020-04-25 17:43:16 -0400

    dfdsf

---
+++ withtest-1.py
@@ -0,0 +1,13 @@
+#test with statement
+a=0;
+with open('C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Scripts\sdj.txt') as f:
+     a=1;
+     f.__exit__()
+print(a);
+
+def __exit__():
+    return "sdssdsadd";
+
+
+def __enter__():
+    return "sdsd";
